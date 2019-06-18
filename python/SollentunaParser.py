1060e9e09a62c13a756cfa90580cfb5313a57882
Robert Lorentz <robey112@live.se>
2019-06-18 11:18:17 +0200

    refactor

--- python/SollentunaParser.py
+++ python/SollentunaParser.py
@@ -1,12 +1,19 @@
 from bs4 import BeautifulSoup
+from Appartment import Appartment
 
-def getAvailableAppartmentsFromSollentuna(page_content):
+def getAvailableAppartmentsFromSollentuna(page_content, provider):
     app_dict = {}
     for index, appart in enumerate(page_content.find_all("article")):
         rent = appart.find("div", {"class": "c-objects__item__price"}).text
         item_info = appart.find("div", {"class": "c-objects__item__info"}).text
         address = appart.find("h1", {"class": "c-objects__item__heading"}).text
+        number_of_people_signed_up = None
+        appartment_url = None
+        available_until = None
+        floor = None
         (rooms, size, location) = getItemInfo(item_info)
+        app_dict[index] = Appartment(location, address, rooms, floor, size, rent, number_of_people_signed_up, 
+            appartment_url, "", provider.provider_string, available_until)
 
 def getItemInfo(info):
     infos = info.split('|')
@@ -15,4 +22,3 @@ def getItemInfo(info):
         size = [int(s) for s in infos[1].split() if s.isdigit()][0]
         location = infos[2].strip()
         return (rooms, size, location)
-print(getItemInfo("asd"))
--- python/_HTMLParser.py
+++ python/_HTMLParser.py
@@ -4,9 +4,10 @@ from bs4 import BeautifulSoup
 from enum import Enum
 from WebScraper import scrapePages
 from SollentunaParser import getAvailableAppartmentsFromSollentuna
+from Appartment import *
 
 class Provider:
-    def __init__(self, request_url, appartment_url, location_index, address_index, rooms_index, size_index, 
+    def __init__(self, provider_string, request_url, appartment_url, location_index, address_index, rooms_index, size_index, 
     floor_index, rent_index, available_until_index, number_of_signed_up, has_duplicates, pathToNextButton="//a[@class='btn next']"):
         self.request_url = request_url
         self.appartment_url = appartment_url
@@ -20,6 +21,9 @@ class Provider:
         self.number_of_signed_up = number_of_signed_up
         self.has_duplicates = has_duplicates
         self.pathToNextButton = pathToNextButton
+        self.provider_string = provider_string
+
+
 
 FORVALTAREN_URL = 'https://www.forvaltaren.se/ledigt/lagenhet'
 HASSELBY_HEM_URL = 'https://bostad.hasselbyhem.se/HSS/Object/object_list.aspx?cmguid=4e6e781e-5257-403e-b09d-7efc8edb0ac8&objectgroup=1'
@@ -28,41 +32,41 @@ VASBY_HEM_URL = 'https://www.vasbyhem.se/ledigt/sok/lagenhet'
 HANINGE_BOSTADER_URL = 'https://minasidor.haningebostader.se/ledigt/sok/lagenhet'
 IKANO_BOSTAD_URL = 'https://hyresratt.ikanobostad.se/ledigt/sok/lagenhet'
 
-FORVALTAREN = Provider(FORVALTAREN_URL, 'https://www.forvaltaren.se/ledigt/', 0, 0, 1, 2, 3, 4, 5, 6, False)
-HASSELBY_HEM = Provider(HASSELBY_HEM_URL, 'https://bostad.hasselbyhem.se/HSS/Object/', 1, 0, 1, 2, None, 3, 4, 5, False)
-SOLLENTUNA_HEM = Provider(SOLLENTUNA_HEM_URL, 'https://www.sollentunahem.se/ledigt/', 0, 1, 1, 2, 3, 4, 5, 6, True)
-VASBY_HEM = Provider(VASBY_HEM_URL, 'https://www.vasbyhem.se/ledigt/detalj/', 1, 1, 2, 3, 4, 5, 6, 7, True)
-HANINGE_BOSTADER = Provider(HANINGE_BOSTADER_URL, 'https://minasidor.haningebostader.se/ledigt/detalj/', 1, 0, 1, 2, None, 3, 4, 5, False)
-IKANO_BOSTAD = Provider(IKANO_BOSTAD_URL, 'https://hyresratt.ikanobostad.se/ledigt/detalj/', 1, 1, 2, 3, 0, 4, 5, None, False)
+FORVALTAREN = Provider("Förvaltaren",FORVALTAREN_URL, 'https://www.forvaltaren.se/ledigt/', 0, 0, 1, 2, 3, 4, 5, 6, False)
+HASSELBY_HEM = Provider("Hässelby Hem",HASSELBY_HEM_URL, 'https://bostad.hasselbyhem.se/HSS/Object/', 1, 0, 1, 2, None, 3, 4, 5, False)
+SOLLENTUNA_HEM = Provider("Sollentuna Hem",SOLLENTUNA_HEM_URL, 'https://www.sollentunahem.se/ledigt/', 0, 1, 1, 2, 3, 4, 5, 6, True)
+VASBY_HEM = Provider("Väsby Hem",VASBY_HEM_URL, 'https://www.vasbyhem.se/ledigt/detalj/', 1, 1, 2, 3, 4, 5, 6, 7, True)
+HANINGE_BOSTADER = Provider("Haninge Bostäder",HANINGE_BOSTADER_URL, 'https://minasidor.haningebostader.se/ledigt/detalj/', 1, 0, 1, 2, None, 3, 4, 5, False)
+IKANO_BOSTAD = Provider("Ikano Bostad",IKANO_BOSTAD_URL, 'https://hyresratt.ikanobostad.se/ledigt/detalj/', 1, 1, 2, 3, 0, 4, 5, None, False)
 
 def getPageContent(provider):
     htmls = scrapePages(provider.request_url, provider.pathToNextButton)
-    app_dicts = []
+    app_list = []
     for html in htmls:
         page_content = BeautifulSoup(html, "html.parser")
-        app_dicts.append(getAvailableAppartmentsFrom(provider, page_content))
-    return app_dicts
+        app_list += getAvailableAppartmentsFrom(provider, page_content)
+    return app_list
 
     
 def getAvailableAppartmentsFrom(provider, page_content):
     if provider == SOLLENTUNA_HEM:
-        return getAvailableAppartmentsFromSollentuna(page_content)
+        return getAvailableAppartmentsFromSollentuna(page_content, provider)
     else:
         return getStandardizedAppartmentsList(page_content, provider)
         
 
 def getStandardizedAppartmentsList(page_content, provider):
     appartments = page_content.find_all("tr")
-    app_dict = {}
-    for index, appart in enumerate(appartments):
+    app_list = []
+    for index, appart_html in enumerate(appartments):
         if index == 0 or (provider.has_duplicates and index % 2 == 0):
             continue
-        app_dict[index] = getPropertiesFromProvider(appart, provider, index)
-    return app_dict
+        app_list.append(getAppartment(appart_html, provider, index))
+    return app_list
 
-def getPropertiesFromProvider(appart, provider, index):
-    spans = appart.find_all('span')
-    a = appart.find_all('a')
+def getAppartment(appart_html, provider, index):
+    spans = appart_html.find_all('span')
+    a = appart_html.find_all('a')
     if len(spans) > 0 and len(a) > 0:
         location = spans[provider.location_index].text
         address = a[provider.address_index].text
@@ -73,19 +77,16 @@ def getPropertiesFromProvider(appart, provider, index):
         available_until = spans[provider.available_until_index].text
         number_of_people_signed_up = spans[provider.number_of_signed_up].text if provider.number_of_signed_up != None else 'null'
         appartment_url = getLink(a, provider)
-        return {
-            "location" : location, "address" : address, "rooms" : rooms, 
-            "floor" : floor, "size" : size, "rent" : rent, "provider": provider, 
-            "appartment_url": appartment_url, "available_until" : available_until, 
-            "number_of_people_signed_up" : number_of_people_signed_up
-        }
+        return Appartment(location, address, rooms, floor, size, rent, number_of_people_signed_up, 
+            appartment_url, "", provider.provider_string,available_until)
+        
     
 
 def getLink(aelem, provider):
     return provider.appartment_url + aelem[provider.address_index]['href']
     
 
-app = getPageContent(SOLLENTUNA_HEM)
+app = getPageContent(HASSELBY_HEM)
 print(app)
 
 
