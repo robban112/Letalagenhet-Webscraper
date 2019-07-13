d5848f7b23374d03ff8e75b771cfd5839eadeeec
Christian <christian.ekenstedt@hotmail.com>
2019-07-13 16:59:01 +0200

    fixed for ubuntu

--- WebScraper.py
+++ WebScraper.py
@@ -13,6 +13,9 @@ import os
 chrome_options = Options()  
 chrome_options.add_argument("--headless")  
 chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+
 driver = webdriver.Chrome(executable_path=os.path.abspath("driver/chromedriver"), options=chrome_options)
 
 def scrapePages(url, pathToNextButton):
